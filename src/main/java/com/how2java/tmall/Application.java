fa9750942763e67ec9bd0d1e5c1b6777efb58691
yslu <469899431@qq.com>
2020-04-19 00:46:55 +0800

    分页+增加

--- src/main/java/com/how2java/tmall/Application.java
+++ src/main/java/com/how2java/tmall/Application.java
@@ -3,8 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
-    //访问的测试地址
-    //http://localhost:8080/tmall_springboot/admin
+    //访问的测试地址           http://localhost:8080/tmall_springboot/admin
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);    
     }
--- src/main/java/com/how2java/tmall/service/CategoryService.java
+++ src/main/java/com/how2java/tmall/service/CategoryService.java
@@ -2,7 +2,11 @@ package com.how2java.tmall.service;
 
 import java.util.List;
 
+import com.how2java.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +15,31 @@ import com.how2java.tmall.pojo.Category;
 
 @Service
 public class CategoryService {
-    @Autowired CategoryDAO categoryDAO;
+    @Autowired
+    CategoryDAO categoryDAO;
 
     public List<Category> list() {
         Sort sort = new Sort(Sort.Direction.DESC, "id");
         return categoryDAO.findAll(sort);
+    }
+
+    /**
+     * 分页查询
+     *
+     * @param start
+     * @param size
+     * @param navigatePages
+     * @return
+     */
+    public Page4Navigator<Category> list(int start, int size, int navigatePages) {
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        Pageable pageable = new PageRequest(start, size, sort);
+        Page pageFromJPA = categoryDAO.findAll(pageable);
+
+        return new Page4Navigator<>(pageFromJPA, navigatePages);
+    }
+
+    public void add(Category bean) {
+        categoryDAO.save(bean);
     }
 }
